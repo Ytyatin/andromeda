b83662fec884fa2544fd7c893cb21558322a2aa2
Ytyatin <153301462+Ytyatin@users.noreply.github.com>
2024-04-06 22:47:46 +0530

    Update ahob.js

--- ahob.js
+++ ahob.js
@@ -1,7 +1,7 @@
 {
     "name": "adihair",
     "description": "For Purchasing Adivasi products",
-    "image": "https://raw.githubusercontent.com/Ytyatin/andromeda/main/ahob.jpg",
+    "image": "https://raw.githubusercontent.com/Ytyatin/andromeda/main/ahob.png",
     "attributes": [
         {
             "trait_type": "Origin",
