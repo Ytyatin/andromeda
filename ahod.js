68f04657a751a223653f5bceeb712c1bc68cf045
Ytyatin <153301462+Ytyatin@users.noreply.github.com>
2024-04-06 17:50:28 +0530

    Create ahod.js

---
+++ ahod.js
@@ -0,0 +1,19 @@
+{
+    "name": "Tabaqui",
+    "description": "NFT Description",
+    "image": "https://github.com/Ytyatin/andromeda/blob/main/wallpaperflare.com_wallpaper%20(1).jpg",
+    "attributes": [
+        {
+            "Collection": "Amulya",
+            "value": "Trait Value"
+        },
+        {
+            "Name": "Trait Name",
+            "value": "Trait Value"
+        },
+        {
+            "Background": "Green",
+            "value": "Trait Value"
+        }
+    ]
+}
